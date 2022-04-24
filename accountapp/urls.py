9b20330becb2bf532f86e761acffefe991204604
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-24 20:16:09 +0900

    django course 26

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accountapp.views import AccountCreateView, hello_world, AccountDetailView, AccountUpdateView
+from accountapp.views import AccountCreateView, hello_world, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,10 @@
 
 from site import USER_BASE
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from accountapp.forms import AccountUpdateForm
 
 from accountapp.models import HelloWorld
@@ -45,3 +46,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
