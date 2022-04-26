aca291a82912b12ce9c4c4fd894f7cd63f7dec46
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-26 10:51:24 +0900

    django course 26

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,13 @@
 
 from site import USER_BASE
 
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 
 from accountapp.models import HelloWorld
@@ -13,6 +16,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
+has_ownership = [ login_required, account_ownership_required ]
+
+@login_required
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
@@ -27,20 +33,19 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
 
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -48,6 +53,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
