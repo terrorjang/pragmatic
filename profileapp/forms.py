44759bca7025ff0b41777507e16fcb201189e5b6
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-26 11:21:48 +0900

    django course 30

---
+++ profileapp/forms.py
@@ -0,0 +1,9 @@
+
+from cProfile import Profile
+from django.forms import ModelForm
+
+
+class ProfileCreationForm(ModelForm):
+    class Meta:
+        model= Profile
+        fields = ['image', 'nickname', 'message']
---
+++ profileapp/urls.py
@@ -0,0 +1,6 @@
+
+appname = 'profileapp'
+
+urlpatterns = [
+    
+]
