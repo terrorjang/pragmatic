4cce28c88931be53f845143b0b07d7ca682e734b
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-27 10:15:07 +0900

    django course 35

---
+++ articleapp/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model=Article
+        fields = ['title', 'image', 'content']
--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from contextlib import nullcontext
+from email.mime import image
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    title = models.CharField(max_length=200, null=True)
+    content = models.TextField(null= True)
+    image = models.ImageField(upload_to='article/', null=False)
+    created_at = models.DateField(auto_now_add=True, null=True)
