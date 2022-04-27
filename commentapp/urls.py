0d5ee3f9b2ff31d3ad9b707f97f715956f707d4b
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-27 14:42:30 +0900

    django course 38

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
+
 ]
