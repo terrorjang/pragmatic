08ee388693b27bf47fe921e953be6ddc78982f44
youngseok.jang <yougnseok.jang@seculetter.com>
2022-04-27 16:19:13 +0900

    django course 40

---
+++ projectapp/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from profileapp.views import ProfileCreateView, ProfileUpdateView
+from projectapp.views import ProjectCreateView, ProjectListView, ProjectDetailView
+
+app_name = 'projectapp'
+
+urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name= 'create'),
+    path('list/', ProjectListView.as_view(), name= 'list'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name= 'detail'),
+    # path('update/<int:pk>', ProfileUpdateView.as_view(), name= 'update'),
+]
